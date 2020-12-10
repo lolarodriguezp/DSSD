6b39bdc3c91e04f2e3ddda78a39ba071c7da5f44
Pilar Acha Altamiranda <pilar@MacBook-Pro-de-Pilar.local>
2020-12-10 16:17:47 -0300

    fix

--- app/Notification.php
+++ app/Notification.php
@@ -10,6 +10,6 @@ class Notification extends Model
     protected $table = 'notificaciones';
 
     protected $fillable = [
-        'id', 'tipo_notificacion' , 'leida', 'id_protocolo'
+        'id', 'tipo_notificacion' , 'leida', 'id_proyecto'
     ];
 }
